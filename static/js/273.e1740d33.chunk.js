e92dd81c392c1eab5d079105e5492ab0f62c9ec7
I-Kolesnyk <106455256+I-Kolesnyk@users.noreply.github.com>
2023-03-12 22:57:04 +0000

    Deploying to gh-pages from @ I-Kolesnyk/goit-react-hw-05-movies@70363e153c68ffcc3e9d19775385720357ee8c91 🚀

--- static/js/273.e1740d33.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[273],{2273:function(e,t,r){r.r(t),r.d(t,{default:function(){return s}});var a=r(1087),n=r(1683),o=r(184);var s=function(){return(0,o.jsxs)("main",{children:[(0,o.jsx)("p",{children:"There are some problems! Try again later."}),(0,o.jsx)("img",{scr:n,alt:"404 page not found"}),(0,o.jsx)(a.OL,{to:"/",text:"Go Home"})]})}},1683:function(e,t,r){e.exports=r.p+"static/media/pageNotFound.40e01b8b2b041b3dcb6f.jpg"}}]);
-//# sourceMappingURL=273.e1740d33.chunk.js.map
